1508e0e3a5e9a7c40540f4ab2f1f6cc6d85769dd
janarthanan1406006 <bjanarthananb@gmail.com>
2026-08-10 18:02:13 +0000

    tuple.py added

---
+++ set.py
@@ -0,0 +1,11 @@
+matclub={"vetri","gokul","ashik","prabu"}
+sciclub={"jana","abi","prabu","gokul"}
+both = (matclub & sciclub)
+only = (matclub | sciclub)
+either = (matclub - sciclub)
+print(both)
+
+print(only)
+
+print(either)
+
