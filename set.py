d4c4a476af84821dab51d7602ba8783483e52184
janarthanan1406006 <bjanarthananb@gmail.com>
2026-08-10 18:21:19 +0000

    modified set.py

--- set.py
+++ set.py
@@ -1,4 +1,4 @@
-matclub={"vetri","gokul","ashik","prabu"}
+"""matclub={"vetri","gokul","ashik","prabu"}
 sciclub={"jana","abi","prabu","gokul"}
 both = (matclub & sciclub)
 only = (matclub | sciclub)
@@ -7,5 +7,14 @@ print(both)
 
 print(only)
 
-print(either)
+print(either)"""
+
+
+"""colours={"blue","green","yellow"}
+print(colours)
+print(type (colours))"""
+
+
+numbers={1,2,3,4,3,2,5,7,5,3,2,4,5}
+print(numbers)
 
