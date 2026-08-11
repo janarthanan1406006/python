903a03594c2054200de415a5cbc76ba7fb44b844
janarthanan1406006 <bjanarthananb@gmail.com>
2026-08-11 16:43:11 +0000

    dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+"""dic={
+"mango":30,
+"apple":40,
+"grapes":60
+}
+print(dic)
+print(dic ['mango'])
+
+print(dic.get('grapes'))"""
+
+
+dic = {"Riya": 88, "Karan": 92, "Meera": 79}
+max=dic(max)
+print(max)
+
---
+++ variable.py
@@ -0,0 +1,9 @@
+"""a=10
+b=10
+print(id(a))
+print(id(b))"""
+
+a="the fucking killing myself and i am weak thats so iukhsdilyhkjhd"
+b="the fucking killing myself srtwsgn dfsbdd "
+print(id(a))
+print(id(b))
