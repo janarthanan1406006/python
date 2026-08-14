016657c60200f3508fccf3d0dbba07d21e0bc28d
janarthanan1406006 <bjanarthananb@gmail.com>
2026-08-14 15:24:55 +0000

    modified

--- module_math.py
+++ module_math.py
@@ -26,3 +26,19 @@ print(" power result ",power_result)
 print(" rounded up ",rounded_up)
 print(" rounded down ",rounded_down)
 
+
+
+
+import math
+
+number=int(input("enter a number : "))
+pow_number1=int(input("enter a power number1 : "))
+pow_number2=int(input("enter a power number : "))
+square_root=math.sqrt(number)
+power_result=math.pow(pow_number1,pow_number2)
+rounded_up=math.ceil(10.7)
+rounded_down=math.floor(12.8)
+print(" square root ",square_root)
+print(" power result ",power_result)
+print(" rounded up ",rounded_up)
+print(" rounded down ",rounded_down)
