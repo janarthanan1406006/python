5a982a0c2f0184ecf19eefeb3e2b33a233f3922a
janarthanan1406006 <bjanarthananb@gmail.com>
2026-08-14 15:18:00 +0000

    module_mat.py

---
+++ module_math.py
@@ -0,0 +1,28 @@
+
+
+import math
+
+number=5
+square_root=math.sqrt(number)
+power_result=math.pow(5,3)
+rounded_up=math.ceil(5.6)
+rounded_down=math.floor(5.6)
+print(" square root ",square_root)
+print(" power result ",power_result)
+print(" rounded up ",rounded_up)
+print(" rounded down ",rounded_down)
+
+
+
+import math
+
+number=15
+square_root=math.sqrt(number)
+power_result=math.pow(5,8)
+rounded_up=math.ceil(10.7)
+rounded_down=math.floor(12.8)
+print(" square root ",square_root)
+print(" power result ",power_result)
+print(" rounded up ",rounded_up)
+print(" rounded down ",rounded_down)
+
