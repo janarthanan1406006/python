a360dc2420d3a23fbd4fb8f9707eb414bb3f41b3
janarthanan1406006 <bjanarthananb@gmail.com>
2026-08-27 15:58:09 +0000

    modified

--- user input.py
+++ user input.py
@@ -2,6 +2,13 @@
 #print(x,"is a beautiful city")
 
 
-num = input(" enter the number: ")
-result = num * 4
-print(result)
+#num =  int (input(" enter the number: "))
+#result = num * 4
+#print(result) 
+
+
+x = int (input("enter the number 1 : "))
+y = int (input("enter the number 2 :"))
+z = x+y
+print(z)
+
