c51034e2af67b8be7a5824cd7cb6fe24e96dc214
janarthanan1406006 <bjanarthananb@gmail.com>
2026-08-28 15:57:40 +0000

    modified

--- if.py
+++ if.py
@@ -10,9 +10,20 @@
 
 
 
-age = 16
+##age = 16
 
-if age >= 18:
-    print("allowed to watch the 18+ movie")
+##if age >= 18:
+    ##print("allowed to watch the 18+ movie")
+##else:
+    ##print("not allowed to watch 18+ movie") 
+
+
+mark = 82
+if mark >= 90:
+    print(" a grade")
+elif mark >= 75 and mark <= 89:
+    print(" b grade")
+elif mark >= 50 and mark <=74:
+    print(" c grade ")
 else:
-    print("not allowed to watch 18+ movie")
+    print(" fail ")           
