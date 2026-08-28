2e81dfff252bcd32bc9fcfe2d565b9db04e94da9
janarthanan1406006 <bjanarthananb@gmail.com>
2026-08-28 17:00:43 +0000

    modified

--- if.py
+++ if.py
@@ -26,4 +26,14 @@ elif mark >= 75 and mark <= 89:
 elif mark >= 50 and mark <=74:
     print(" c grade ")
 else:
-    print(" fail ")           
+    print(" fail ")  
+
+
+
+num = int (input(" enter the number: "))
+if num < 0:
+    print(" positive number ")
+elif num > 0:
+    print(" negative number ")
+else:
+    print(" zero ")                 
