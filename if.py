1b26e04bbdbebb19a0e2c425b77548a49c524b47
janarthanan1406006 <bjanarthananb@gmail.com>
2026-08-28 15:49:39 +0000

    modified

--- if.py
+++ if.py
@@ -1,7 +1,18 @@
-num = 15
-if num > 0:
-    print("positive number")
-elif num < 0:
-    print("negative number")
+##num = 15
+##if num > 0:
+  ##  print("positive number")
+##elif num < 0:
+  ##  print("negative number")
+##else:
+  ##  print("zero")       
+
+
+
+
+
+age = 16
+
+if age >= 18:
+    print("allowed to watch the 18+ movie")
 else:
-    print("zero")        
+    print("not allowed to watch 18+ movie")
