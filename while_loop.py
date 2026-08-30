038b0cb91f5db2cc7899ce86f9c244be24500b98
janarthanan1406006 <bjanarthananb@gmail.com>
2026-08-30 15:37:54 +0000

    modified

--- while_loop.py
+++ while_loop.py
@@ -1,4 +1,26 @@
 i= 1
 while i <= 10:
     print(i)
-    i += 2 
+    i += 2
+
+
+
+i = 10
+while i >= 0:
+    print(i)
+    i -= 1 
+
+
+
+
+a = 0
+while True:
+   i = int(input("Enter a number: "))
+   a = a + i
+   if i == 0:
+      break
+print(a)   
+   
+
+ 
+    
