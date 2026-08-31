ff1284742ec2e9ec2ce8c5054a06af409baf0cb8
janarthanan1406006 <bjanarthananb@gmail.com>
2026-08-31 17:26:09 +0000

    modified

--- example while_loop.py
+++ example while_loop.py
@@ -1,6 +1,13 @@
 ##beginner##
 
-count = 1
+"""count = 1
 while count <= 5:
     print(count)
-    count = count + 1
+    count = count + 1"""
+
+
+n = int(input("Enter an age: "))
+while n<0:
+    print( "invalid age" , n)
+    n = int(input("Enter an age: "))
+    print("current age is", n)    
