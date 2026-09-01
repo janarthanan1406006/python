02f7a26333c97b2f168a76e08b508e710ffedda0
janarthanan1406006 <bjanarthananb@gmail.com>
2026-09-01 16:32:29 +0000

    modified

--- for_loop.py
+++ for_loop.py
@@ -10,8 +10,19 @@ for y in fruits:
 
 ## intermediate problem ##
 
-num = [1,2,3,4,5,6,7,8,]
+"""num = [1,2,3,4,5,6,7,8,]
 for x in num:
 
     if x % 2 == 0:
-        print("even number", x)
+        print("even number", x)"""
+
+
+
+## advanced ##
+
+for i in range(2,50):
+    for j in range(2,i):
+        if i % j == 0:
+            break
+    else:
+        print(i)    
