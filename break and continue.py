6c4621ad44e3290386e226ec7d0966ab239158e7
janarthanan1406006 <bjanarthananb@gmail.com>
2026-09-03 16:41:16 +0000

    modified

--- break and continue.py
+++ break and continue.py
@@ -1,7 +1,26 @@
-for i in range(1,10):
+"""for i in range(1,10):
     if i % 3 == 0:
         continue
-    print(i)
+    print(i)"""
 
 
-    
+
+
+"""for x in range(1, 20):
+    if x == 14:
+        break
+    print(x)"""
+
+
+
+correct_password = "xxx"
+for attempt in range(3):
+    password = input("enter the password: ")
+    if password == correct_password:
+        print("acces granted")
+        break
+    else:
+        print("incorrect pasword, tryagain") 
+
+
+
