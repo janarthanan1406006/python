60d45f4ea8c0f3be160be83854ba9d31874b7426
janarthanan1406006 <bjanarthananb@gmail.com>
2026-09-03 16:53:34 +0000

    modified

--- break and continue.py
+++ break and continue.py
@@ -13,14 +13,34 @@
 
 
 
-correct_password = "xxx"
+"""correct_password = "xxx"
 for attempt in range(3):
     password = input("enter the password: ")
     if password == correct_password:
         print("acces granted")
         break
     else:
-        print("incorrect pasword, tryagain") 
+        print("incorrect pasword, tryagain")""" 
 
 
+"""number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for x in number:
+    if x % 2 == 0:
+        print(" first even number", x)
+        break"""
+
+
+
+num = 17
+is_prime = True
+for i in range(2, num):
+    if num % i == 0:
+        is_prime = False
+        break
+if is_prime:
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
+
+    
 
