0cfa42f32c110b5c901b15e487b51f4917223fed
janarthanan1406006 <bjanarthananb@gmail.com>
2026-09-07 16:36:19 +0000

    modified

--- array.py
+++ array.py
@@ -1,3 +1,13 @@
-from array import * 
+"""from array import * 
 arr1 = array('i', [33,44,55,6,3,4,56,7,67])
-print(type(arr1))
+print(type(arr1))"""
+
+
+from array import *
+arr1 = array('f', [2.5, 4.8, -3.2, 6.7])
+
+for i in arr1:
+    print(i)
+    if i < 0:
+        print("negative number is not allowed")
+        continue
