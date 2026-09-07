9df70e93c7b5cd537e3510294f70de59aa022456
janarthanan1406006 <bjanarthananb@gmail.com>
2026-09-07 17:54:10 +0000

    string method.py

---
+++ string method.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+
+result = len(name)
+result = name.capitalize()
+result = name.upper()
+result = name.lower()
+result = name.isalpha()
+result = name.isdigit()
+print("Result:", result)
+    
