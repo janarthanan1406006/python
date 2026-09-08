76345756c5016eedfb6e5a1e537b4d6bfa32196a
janarthanan1406006 <bjanarthananb@gmail.com>
2026-09-08 16:51:15 +0000

    modified

--- array_function.py
+++ array_function.py
@@ -1,7 +1,7 @@
 """from array import *
 arr1 = array('i', [20,3,9,900,76,87,45])
 arr1.append(25)
-print(arr1)"""
+print(arr1)
 
 from array import *
 arr1 = array('i', [20,3,9,900,76,87,45])
@@ -10,7 +10,18 @@ print(result)
 
 
 
-"""from array import *
+from array import *
 arr1 = array('i', [20,3,9,900,76,87,45])
 arr1.reverse()
 print(arr1)"""
+
+
+
+from array import *
+arr1 = array('i', [20,3,9,900,76,87,45])
+arr1.insert(2, 800)
+arr1.remove(9)
+result = arr1.index(76)
+arr1.extend([100,200,300])
+print(arr1)
+print(result)
