22caf96258c21fed4f522ed23f09b760b367f51d
janarthanan1406006 <bjanarthananb@gmail.com>
2026-09-09 15:54:34 +0000

    modified

--- function_arg.py
+++ function_arg.py
@@ -11,11 +11,23 @@ def add(*scores):
     print(scores)
     print(sum(scores))
 add (80,65,43,67,56)
-add(50,54,76)""" 
+add(50,54,76)
 
 
 def add(*names):
     print(names)
-add ("asha","jana","vetri","santhosh")
+add ("asha","jana","vetri","santhosh")"""
 
 
+#keyword arguments
+
+"""def student_info(**student_details):
+    for key,value in student_details.items():
+        print(f"{key}:{value}")
+student_info(name = "jana" , age = 20 , city = "sivaganga")"""      
+    
+
+def grocery_list(**grocery_product):
+        for key,value in grocery_product.items():
+            print(f"{key}:{value}")
+grocery_list(tomato = "5kg" , carrot = "2kg", cucumber = "5pc")
