090ac2023009991c83c3ac477ecf223378fc7290
janarthanan1406006 <bjanarthananb@gmail.com>
2026-09-09 15:39:03 +0000

    modified

--- function_arg.py
+++ function_arg.py
@@ -1,6 +1,19 @@
 #default argument
 
-def hello (name, text = " welcome to python"):
+"""def hello (name, text = " welcome to python"):
     print(f"{name},{text}")
 hello("jana")
-hello("asha" , "welcome to vs code")    
+hello("asha" , "welcome to vs code")   
+
+#variable length parameter
+
+def add(*scores):
+    print(scores)
+    print(sum(scores))
+add (80,65,43,67,56)
+add(50,54,76)""" 
+
+
+
+
+
