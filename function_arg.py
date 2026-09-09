a1250e999e417d2458dfdd6749287cc57b74fd81
janarthanan1406006 <bjanarthananb@gmail.com>
2026-09-09 16:06:22 +0000

    modified

--- function_arg.py
+++ function_arg.py
@@ -24,10 +24,19 @@ add ("asha","jana","vetri","santhosh")"""
 """def student_info(**student_details):
     for key,value in student_details.items():
         print(f"{key}:{value}")
-student_info(name = "jana" , age = 20 , city = "sivaganga")"""      
+student_info(name = "jana" , age = 20 , city = "sivaganga")     
     
 
 def grocery_list(**grocery_product):
         for key,value in grocery_product.items():
             print(f"{key}:{value}")
-grocery_list(tomato = "5kg" , carrot = "2kg", cucumber = "5pc")
+grocery_list(tomato = "5kg" , carrot = "2kg", cucumber = "5pc")"""
+
+
+
+#positional argument
+
+def positional_argument(animal_type , pet_name):
+    print(f"i have a {animal_type} named {pet_name}.")
+positional_argument("dog" , "rocky")    
+  
