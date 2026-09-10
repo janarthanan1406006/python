0629cf9dffcc2cb944fcea8a012a3af14520cb8e
janarthanan1406006 <bjanarthananb@gmail.com>
2026-09-10 17:03:14 +0000

    modified

--- local _global_var.py
+++ local _global_var.py
@@ -1,10 +1,17 @@
-a = 10
+"""a = 10
 
 def nothing():
  x = 15
  print("inside variable : ",x )
 nothing()
 
-print("outside variable :" , a)
+print("outside variable :" , a)"""
+
  
- 
+def add_numbers():
+    x = 3
+    y = 4
+    result = x + y
+    print(result)
+add_numbers() 
+print(result)     
